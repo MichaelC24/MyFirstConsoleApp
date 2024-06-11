4e1850cc6db42ee52f8ff9c5f1127595600ba84c
Michael Clemons <Mclemons@tql.com>
2024-06-11 14:50:22 -0400

    Add project files.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MyFirstConsoleApp
+{
+    internal class Program
+    {
+        public static string Name { get; set; } = "greg";
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"Hello,{Name} World!");
+        }
+    }
+}
